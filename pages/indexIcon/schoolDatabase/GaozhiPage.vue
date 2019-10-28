96dd2dd7d6b5b3c79a1ab6da32bcfb019cf690f1
wcz <15895903773@163.com>
2019-10-28 18:27:54 +0800

    院校库首页开发完毕

---
+++ pages/indexIcon/schoolDatabase/GaozhiPage.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="">
+		本科层次职业教育
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/indexIcon/schoolDatabase/SchoolList.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="list">
+		<view class="list-item" 
+			v-for="(item,index) in listArr" 
+			:key="index">
+			<view class="left">
+				{{item.title.substr(0,1)}}
+			</view>
+			<view class="right">
+				<view class="">
+					{{item.title}}
+				</view>
+				<scroll-view  scroll-y="true" >
+					<view class="tag"
+					v-for="(tag,i) in item.tags" :key="i">
+						{{tag.name}}
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			listArr:{
+				type:Array,
+				required:true,
+			}
+		},
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.list-item{
+		padding: 30upx;
+		// display: flex;
+		// align-items: center;
+		border-bottom: solid 1px #797979;
+		.left{
+			display: inline-flex;
+			width:120upx;
+			height:120upx;
+			border-radius: 60upx;
+			background: #4CD964;
+			vertical-align: middle;
+			align-items: center;
+			justify-content: center;
+			font-size: 50upx;
+		}
+		.right{
+			display: inline-block;
+			width: 550upx;
+			vertical-align: middle;
+			margin-left: 20upx;
+			.tag{
+				display: inline-block;
+				font-size: 20upx;
+				padding: 5upx 10upx;
+				border-radius: 10upx;
+				border: solid 1px #797979;
+				margin: 10upx  10upx 0 10upx;
+			}
+		}
+	}
+</style>
