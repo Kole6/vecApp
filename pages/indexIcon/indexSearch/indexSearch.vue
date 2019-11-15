8aaca1f71f02907d6da5f157d97badacaad88628
wy333 <1577653650@qq.com>
2019-11-15 16:47:59 +0800

    首页搜索

--- pages/indexIcon/indexSearch/indexSearch.vue
+++ pages/indexIcon/indexSearch/indexSearch.vue
@@ -6,26 +6,35 @@
 		<view class="have-data">
 			<QSTabs ref="tabs" :current="current" :tabs="tabs" width="250" swiperWidth="750" activeColor="#6451FC"
 			 backgroundColor="#fff" @change="change($event)" />
-			<swiper :style="{height:`${scrollH - 200}upx`,borderTop: '1upx solid rgba(238, 238, 238, 0.3)'}" :current="current"
+			<swiper :style="{height:`${scrollH - 185}upx`,borderTop: '1upx solid rgba(238, 238, 238, 0.3)'}" :current="current"
 			 @change="swiperChange" @transition="transition" @animationfinish="animationfinish">
 				<swiper-item class="swiper-item">
 					<scroll-view scroll-y style="height: 100%;">
-						<view v-for="(ite, ind) in 30" :key="ind" class="sc-it" hover-class="navigator-hover" @tap="toSchoolDetail()">
-							上海电子职业技术学院{{ind+1}}
+						<view class="list-title">
+							<p>院校</p>
+						</view>
+						<view class="school-list" >
+							<school-list :isText="true" :showType="4" :is-special="true" :listArr="dataArr2" :handleTaped="handleListTaped"></school-list>
+						</view>
+						<view class="list-title">
+							<p>专业</p>
+						</view>
+						<view class="school-list">
+							<school-list :isText="true" :showType="4" :listArr="dataArr"></school-list>
 						</view>
 					</scroll-view>
 				</swiper-item>
 				<swiper-item class="swiper-item">
 					<scroll-view scroll-y style="height: 100%;">
-						<view v-for="(ite, ind) in 30" :key="ind" class="sc-it" hover-class="navigator-hover" @tap="toSchoolDetail()">
-							计算机应用专业{{ind+1}}
+						<view class="school-list">
+							<school-list :isText="true" :showType="4" :listArr="dataArr"></school-list>
 						</view>
 					</scroll-view>
 				</swiper-item>
 				<swiper-item class="swiper-item">
 					<scroll-view scroll-y style="height: 100%;">
-						<view v-for="(ite, ind) in 30" :key="ind" class="sc-it" hover-class="navigator-hover" @tap="toSchoolDetail()">
-							计算机应用专业{{ind+1}}
+						<view class="school-list">
+							<school-list :isText="true" :showType="4" :listArr="dataArr"></school-list>
 						</view>
 					</scroll-view>
 				</swiper-item>
@@ -37,15 +46,80 @@
 <script>
 	import uniSearchBar from "@/components/uni-search-bar/uni-search-bar.vue";
 	import QSTabs from '@/components/QS-tabs/QS-tabs.vue';
+	import schoolList from '@/pages/indexIcon/schoolDatabase/SchoolList';
 	export default {
 		components: {
 			uniSearchBar,
-			QSTabs
+			QSTabs,
+			schoolList
 		},
 		data() {
 			return {
-				tabs: ["全部","学校", "专业"],
-				current: 0
+				tabs: ["全部","院校","专业"],
+				current: 0,
+				dataArr: [
+					{
+						title: '北京电子科技职业技术学院',
+						tags: [{ name: '地区', value: '上海' }, { name: '层次', value: '高职' }],
+						cards: [{ name: '民办' }, { name: '本科层次职业教育' }]
+					},
+					{
+						title: '北京电子科技职业技术学院',
+						tags: [{ name: '地区', value: '上海' }, { name: '层次', value: '高职' }],
+						cards: [{ name: '民办' }, { name: '本科层次职业教育' }]
+					},
+					{
+						title: '北京电子科技职业技术学院',
+						tags: [{ name: '地区', value: '上海' }, { name: '层次', value: '高职' }],
+						cards: [{ name: '民办' }, { name: '本科层次职业教育' }]
+					},
+					{
+						title: '北京电子科技职业技术学院',
+						tags: [{ name: '地区', value: '上海' }, { name: '层次', value: '高职' }],
+						cards: [{ name: '民办' }, { name: '本科层次职业教育' }]
+					},
+					{
+						title: '北京电子科技职业技术学院',
+						tags: [{ name: '地区', value: '上海' }, { name: '层次', value: '高职' }],
+						cards: [{ name: '民办' }, { name: '本科层次职业教育' }]
+					},
+					{
+						title: '北京电子科技职业技术学院',
+						tags: [{ name: '地区', value: '上海' }, { name: '层次', value: '高职' }],
+						cards: [{ name: '民办' }, { name: '本科层次职业教育' }]
+					}
+				],
+				dataArr2: [
+					{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					},{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					},{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					},{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					},{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					},{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					},{
+						title: '汽车运用与维护',
+						tags: [{ name: '专业大类', value: '交通运输类' }, { name: '代码', value: '0825001234' }],
+						cards:[{name:'学历层次',value:'高职'},{name:'专业年限',value:'3年'}]
+					}
+				]
 			}
 		},
 		computed: {
@@ -63,6 +137,9 @@
 					content: '搜索：' + res.value,
 					showCancel: false
 				})
+			},
+			handleListTaped(){
+				
 			},
 			change(index) {
 				this.current = index;
@@ -106,5 +183,17 @@
 				border-top: 17upx solid #F6F8FE;
 			}
 		}
+	}
+	.list-title{
+		padding-left: 30upx;
+		height: 98upx;
+		font-size:32upx;
+		font-weight:700;
+		color:rgba(51,51,51,1);
+		line-height:98upx;
+	}
+	.school-list {
+		background: #ffffff;
+		overflow: auto;
 	}
 </style>
