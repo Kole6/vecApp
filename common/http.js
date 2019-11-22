e399fbf3c5ae175fa50e60f6b2d6f1f09e24a0a4
wcz <15895903773@163.com>
2019-11-22 10:42:07 +0800

    fix : request请求修改

--- common/http.js
+++ common/http.js
@@ -2,6 +2,12 @@ import {
 	baseURL
 } from '../config'
 const http = (options) => {
+	let header = {}
+	if(typeof options.header === 'object'){
+		header = {
+			...options.header
+		}
+	}
 	return new Promise((resolve, reject) => {
 		uni.showLoading({
 			title: '加载中...',
@@ -9,10 +15,13 @@ const http = (options) => {
 		});
 		uni.request({
 			url: (options.baseURL ? options.baseURL : baseURL) + options.url,
-			method: options.method,
+			// 默认为POST请求
+			method: options.method || 'POST',
 			data: options.data,
 			header: {
-				'v-token': '333333'
+				'v-token': '333333',
+				...header
+				
 			},
 			success: res => {
 				resolve(res.data)
@@ -39,9 +48,12 @@ export default http
 //调用例子
 /*
 this.$HTTP({
-  methods: 'get',
+  method: 'GET',
   url: 'https://unidemo.dcloud.net.cn/api/news',
   data: {},
+  header:{
+	  'content-type':'application/x-www-form-urlencoded'
+  },
   load:true
 }).then((data) =>{
   console.log(data)
--- pages/schoolDetails/SchoolLeader/SchoolLeader.vue
+++ pages/schoolDetails/SchoolLeader/SchoolLeader.vue
@@ -1,13 +1,18 @@
 <template>
 	<view class="">
-		<load-more ref="scroll" @onPullDown="onPullDown" @onScroll="onScroll" @onLoadMore="onLoadMore" :styleObj="{ height: systemInfo.screenHeight - 80 +'px'}" :loadStatus="loadStatus">
-			<view class="m-list" v-for="(item,index) in dataArr" :key="index">
-				<view class="avatar">{{item.teachername.substr(0,1)}}</view>	
-				<view class="name">{{item.teachername}}</view>
+		<load-more
+			ref="scroll"
+			@onPullDown="onPullDown"
+			@onScroll="onScroll"
+			@onLoadMore="onLoadMore"
+			:styleObj="{ height: systemInfo.screenHeight - 80 + 'px' }"
+			:loadStatus="loadStatus"
+		>
+			<view class="m-list" v-for="(item, index) in dataArr" :key="index">
+				<view class="avatar">{{ item.teachername.substr(0, 1) }}</view>
+				<view class="name">{{ item.teachername }}</view>
 				<view class="position">
-					<text v-for="(tag,i) in item.tags" :key="i">
-						{{tag}}
-					</text>
+					<text v-for="(tag, i) in item.tags" :key="i">{{ tag }}</text>
 				</view>
 			</view>
 		</load-more>
@@ -15,128 +20,129 @@
 </template>
 
 <script>
-	import loadMore from '@/components/loadMore/you-scroll.vue'
-	export default {
-		components:{loadMore},
-		data(){
-			return{
-				loadStatus:'noMore',
-				systemInfo: uni.getSystemInfoSync(),
-				page:{
-					pageIndex:1,
-					pageSize:10,
-					sid:'',
-				},
-				dataArr:[],
-			}
+import loadMore from '@/components/loadMore/you-scroll.vue';
+export default {
+	components: { loadMore },
+	data() {
+		return {
+			loadStatus: 'noMore',
+			systemInfo: uni.getSystemInfoSync(),
+			page: {
+				pageIndex: 1,
+				pageSize: 10,
+				sid: ''
+			},
+			dataArr: []
+		};
+	},
+	onLoad(params) {
+		this.page.sid = params.sid || '3633000526';
+		this.getData();
+	},
+	methods: {
+		onPullDown(done) {
+			this.getData().then(() => {
+				done();
+			});
 		},
-		onLoad(params){
-			this.page.sid = params.sid || '3633000526'
-			this.getData()
+		onLoadMore() {
+			this.loadStatus = 'loading';
+			this.getData();
 		},
-		methods:{
-			onPullDown(done){
-				this.getData().then(()=>{
-					done()
+		getData() {
+			return new Promise((resolve, reject) => {
+				this.$HTTP({
+					url: '/zjq/College/GetLeaderInfo',
+					method: 'POST',
+					data: {
+						sid: this.page.sid,
+						token: 'd05902562e544db29bbe777954d43bb0',
+						pageIndex: this.page.pageIndex,
+						pageSize: this.page.pageSize
+					},
+					header:{
+						'content-type':'application/x-www-form-urlencoded'
+					},
 				})
-			},
-			onLoadMore(){
-				this.loadStatus = 'loading'
-				this.getData()
-			},
-			getData(){
-				return new Promise((resolve,reject)=>{
-					uni.request({
-						url:'http://47.103.69.156:18089/zjq/College/GetLeaderInfo',
-						header:{
-							'content-type':'application/x-www-form-urlencoded'
-						},
-						data:{
-							sid:this.page.sid,
-							token:'d05902562e544db29bbe777954d43bb0',
-							pageIndex:this.page.pageIndex,
-							pageSize:this.page.pageSize
-						},
-						method:'POST',
-						success:({data}) => {
-							if(data.code == 0){
-								let arr = data.data.list.map(item=>{
-									return{
-										...item,
-										tags:item.positon.split('、')
-									}
-								})
-								this.dataArr = arr;
-								// 是否为最后一页
-								if(data.data.lastPage){
-									this.loadStatus = 'noMore'
-								}else{
-									this.page.pageIndex++;
-									this.loadStatus = 'more'
-								}
-							}else{
-								this.loadStatus = 'noMore'
-								uni.showModal({
-									content:data.message,
-									icon:'none'
-								})
+					.then((data) => {
+						if (data.code == 0) {
+							let arr = data.data.list.map(item => {
+								return {
+									...item,
+									tags: item.positon.split('、')
+								};
+							});
+							this.dataArr = arr;
+							// 是否为最后一页
+							if (data.data.lastPage) {
+								this.loadStatus = 'noMore';
+							} else {
+								this.page.pageIndex++;
+								this.loadStatus = 'more';
 							}
-						},
-						complete() {
-							resolve();
+						} else {
+							this.loadStatus = 'noMore';
+							uni.showModal({
+								content: data.message,
+								icon: 'none'
+							});
 						}
+						resolve()
 					})
-				})
-			},
+					.catch(err => {
+						reject()
+					});
+			});
 		}
 	}
+};
 </script>
 
 <style scoped lang="scss">
-	.m-list{
-		padding: 10upx 30upx;
-		display: flex;
+.m-list {
+	padding: 10upx 30upx;
+	display: flex;
+	align-items: center;
+	background: #ffffff;
+	border-bottom: solid 1upx $main-dividing-line1;
+	font-size: $uni-font-size-lg;
+	&:first-child {
+		border-top: solid 1upx $main-dividing-line1;
+	}
+	.avatar {
+		box-sizing: border-box;
+		display: inline-flex;
+		width: 120upx;
+		height: 120upx;
+		border-radius: 60upx;
+		background: #ffffff;
+		border: solid 1px #68bcf5;
+		vertical-align: middle;
 		align-items: center;
-		background: #FFFFFF;
-		border-bottom: solid 1upx $main-dividing-line1;
+		justify-content: center;
+		font-size: 45upx;
+		color: #fff;
+		background-color: #68bcf5;
+	}
+	.name {
+		margin-left: 20upx;
 		font-size: $uni-font-size-lg;
-		&:first-child{
-			border-top: solid 1upx $main-dividing-line1;
-		}
-		.avatar{
+		color: $main-text-color;
+	}
+	.position {
+		text {
+			margin-left: 20upx;
 			box-sizing: border-box;
 			display: inline-flex;
-			width:120upx;
-			height:120upx;
-			border-radius: 60upx;
-			background: #FFFFFF;
-			border: solid 1px #68BCF5;
-			vertical-align: middle;
 			align-items: center;
 			justify-content: center;
-			font-size: 45upx;
-			color: #fff;
-			background-color: #68BCF5;
-		}
-		.name{
-			margin-left: 20upx;
-			font-size: $uni-font-size-lg;
-			color: $main-text-color;
-		}
-		.position{
-			text{
-				margin-left: 20upx;
-				box-sizing: border-box;
-				display: inline-flex;
-				align-items: center;
-				justify-content: center;
-				padding: 2upx 11upx;
-				border-radius: 5upx;
-				background: #6451FC;
-				color: #FFFFFF;
-				// border: solid 1upx $main-base-color;
-				font-size: $uni-font-size-base;				
-			}
+			padding: 2upx 11upx;
+			border-radius: 5upx;
+			background: #6451fc;
+			color: #ffffff;
+			// border: solid 1upx $main-base-color;
+			font-size: $uni-font-size-base;
 		}
 	}
+}
 </style>
