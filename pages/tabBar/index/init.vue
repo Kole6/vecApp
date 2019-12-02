3b55a5d17b9ddef191a480da9e47a9cc0b8a4945
wy333 <1577653650@qq.com>
2019-12-02 21:07:38 +0800

    还原

--- pages/tabBar/index/init.vue
+++ pages/tabBar/index/init.vue
@@ -5,19 +5,19 @@
 <script>
 	export default {
 		onLoad() {
-			uni.switchTab({
-				url: './index'
-			});
+			// uni.switchTab({
+			// 	url: './index'
+			// });
 			// 引导页，从本地缓存中同步获取指定 key 对应的内容，用于判断是否是第一次打开应用
-			// if (uni.getStorageSync('launchFlag')) {
-			// 	uni.switchTab({
-			// 		url: './index'
-			// 	});
-			// } else {
-			// 	uni.redirectTo({
-			// 		url: './guide'
-			// 	});
-			// }
+			if (uni.getStorageSync('launchFlag')) {
+				uni.switchTab({
+					url: './index'
+				});
+			} else {
+				uni.redirectTo({
+					url: './guide'
+				});
+			}
 		}
 	}
 </script>
