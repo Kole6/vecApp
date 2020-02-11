7355f2efc9e7d72d0862d87b98e010525572fe83
wy333 <1577653650@qq.com>
2020-02-11 15:34:05 +0800

    1.2.5

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 export const ConfigBaseURL = 'http://47.103.69.156:18089'
 
 //版本
-export const ConfigEdition = '1.2.4'
+export const ConfigEdition = '1.2.5'
 
 //对比列表数量
 export const ConfigContrast = 2
